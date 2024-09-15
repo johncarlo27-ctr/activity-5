12ddc366900cd6ae0fa4e7f3f47c173a3c48a0e3
unknown <johncarloaltura705@gmail.com>
2024-09-15 16:34:27 +0800

    Initial commit with strings.js

---
+++ strings.js
@@ -0,0 +1,14 @@
+let favactorfirstname = "Ivana";
+let favactorlastname = "Alawi";
+let fullname = favactorfirstname + " " + favactorlastname;
+console.log(favactorfirstname);
+console.log(favactorlastname);
+console.log(fullname);
+ 
+let uppercase = "ivana alawi";
+console.log(uppercase.toUpperCase());
+let lowercase = "CICI";
+console.log(lowercase.toLowerCase());
+
+let message = "My favorite actress is " + (fullname.toUpperCase()) + " her best show is Batang Quiapo";
+console.log(message);
